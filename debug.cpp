de06be4d064eafe5adf5fab648e1b8bfe76ce697
Rohnin Barrete <rohnin.barrette@mths.ca>
2021-09-15 17:43:48 +0000

    This program shows how local and global variables work

---
+++ debug.cpp
@@ -0,0 +1,38 @@
+// Copyright [2021] Rohnin Barrette"  [legal/copyright]
+//
+// Created by: Rohnin Barrette
+// Created on: Sept 2021
+// This program calculates the cost of a pizza when given diameter
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+
+void globalVariable() {
+    // this shows what happens with global variables
+
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+
+    localVariable();
+    globalVariable();
+}
